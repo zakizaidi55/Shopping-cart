daa519223eb00ef50d973c79145d5ced62cdd7ae
zakizaidi55 <zakizaidi55@gmail.com>
2023-04-14 01:04:55 +0530

    changes the checkout button css.

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -33,11 +33,13 @@ const Cart = () => {
           <div className="font-semibold text-xl text-green-800 ">Your Cart</div>
           <div className="font-semibold text-5xl text-green-700  -mt-5">Summary</div>
           <p>
-          <span>Total Item:{cart.length}</span>
+          <span className="text-gray-700 font-semibold text-xl">Total Item:{cart.length}</span>
           </p>
           </div>
-          <div>
-          <p>Total Amount: ${totalAmount} </p>
+
+          {/* checkout button and total price */}
+          <div className="flex flex-col"> 
+          <p className="text-xl font-bold">Total Amount: ${totalAmount} </p>
           <button className="bg-green-700 hover:bg-purple-50 rounded-lg text-white transition duration-300 ease-linear mt-5 border-2 border-green-600 font-bold hover:text-green-700 p-3 text-xl">
             Checkout
           </button>
